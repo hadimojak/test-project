212d457f8dd28aa2d8fb429d8d624fbd8e116334
hadimojak <hadimojak@gmail.com>
2022-06-12 13:40:37 +0430

    refactored v1

--- client2.js
+++ client2.js
@@ -6,10 +6,6 @@ const socket = new WebSocket("ws://localhost:8080", {
   },
 });
 
-socket.addEventListener("open", (event) => {
-  socket.send("Hello Server!");
-});
-
 socket.addEventListener("message", function (event) {
   console.log("Message from server ", JSON.parse(event.data));
 });
